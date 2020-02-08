08b57c58e10401c5312ce4d48ee8acbd4990c566
Amber Brookhuis <amberbrookhuis@gmail.com>
2020-02-08 17:37:27 +0100

    check if snake is within grid

--- game.ts
+++ game.ts
@@ -1,6 +1,6 @@
 import { Snake } from './types'
 
-const SPEED = 500
+const SPEED = 100
 
 type Move = { x: number; y: number }
 type Position = { x: number; y: number }
@@ -14,20 +14,40 @@ const canvas = document.getElementById('canvas') as HTMLCanvasElement
 const context = canvas.getContext('2d')
 
 interface State {
-  cols: number;
-  rows: number;
-  snake: Snake[];
-  moves: Move[];
+  cols: number
+  rows: number
+  snake: Snake[]
+  moves: Move[]
+  apple: Position
 }
 
 const initialState = (): State => ({
   cols: 30,
   rows: 20,
   snake: [{ x: 6, y: 6 }],
-  moves: [RIGHT]
+  moves: [RIGHT],
+  apple: { x: 10, y: 6 }
 })
 
 let state = initialState()
+
+const removeFirst = (x: Move[]): Move[] => x.slice(1)
+const removeLast = (x: Move[]): Move[] => x.slice(0, x.length - 1)
+const random = (min: number) => (max: number) =>
+  Math.ceil(Math.random() * (max - min) + min)
+
+const randomPos = (state: State) => ({
+  x: random(0)(state.cols - 1),
+  y: random(0)(state.rows - 1)
+})
+
+const withinGrid = (newPos: number) => (boundaries: number): number => {
+  if (newPos >= boundaries) {
+    return 0
+  } else if (newPos < 0) {
+    return boundaries
+  } else return newPos
+}
 
 const x = (x1: number): number => Math.round(x1 * (canvas.width / state.cols))
 const y = (y1: number): number => Math.round(y1 * (canvas.height / state.rows))
@@ -35,44 +55,58 @@ const y = (y1: number): number => Math.round(y1 * (canvas.height / state.rows))
 const draw = (): void => {
   if (context) {
     // Canvas
-    context.fillStyle = 'blue'
+    context.fillStyle = '#004777'
     context.fillRect(0, 0, canvas.width, canvas.height)
 
     // Snake
-    context.fillStyle = 'white'
+    context.fillStyle = '#00AFB5'
     state.snake.map(cell => context.fillRect(x(cell.x), y(cell.y), x(1), y(1)))
+
+    // Apple
+    context.fillStyle = '#FF7700'
+    context.fillRect(x(state.apple.x), x(state.apple.y), x(1), y(1))
   }
 }
-
-const positionExist = (p1: Position) => (p2: Position): boolean =>
-  p1.x === p2.x && p2.y === p2.y
-
-const nextHead = (state: State): Snake => ({
-  x: state.snake[0].x + state.moves[state.moves.length - 1].x,
-  y: state.snake[0].y + state.moves[state.moves.length - 1].y
-})
-
-const crash = (state: State): boolean => {
-  return false
-}
-
-const removeFirst = (x: Move[]): Move[] => x.slice(1);
-const removeLast = (x: Move[]): Move[] => x.slice(0, x.length - 1);
 
 const isValidMove = (state: State) => (move: Move): boolean =>
   move.x + state.moves[0].x !== 0 || move.y + state.moves[0].y !== 0
 
-const nextSnake = (state: State): Snake[] =>
-  crash(state) ? [] : [nextHead(state)].concat(removeLast(state.snake))
+const positionExist = (p1: Position) => (p2: Position): boolean =>
+  p1.x === p2.x && p1.y === p2.y
 
-const nextMoves = (state: State): Move[] => state.moves.length > 1
-  ? removeFirst(state.moves)
-  : state.moves
+const nextHead = (state: State): Snake => ({
+  x: withinGrid(state.snake[0].x + state.moves[state.moves.length - 1].x)(
+    state.cols
+  ),
+  y: withinGrid(state.snake[0].y + state.moves[state.moves.length - 1].y)(
+    state.rows
+  )
+})
+
+const nextApple = (state: State) =>
+  willEat(state) ? randomPos(state) : state.apple
+
+const willCrash = (state: State) =>
+  state.snake.find(positionExist(nextHead(state)))
+
+const willEat = (state: State) => positionExist(nextHead(state))(state.apple)
+
+const nextSnake = (state: State): Snake[] => {
+  return willCrash(state)
+    ? []
+    : willEat(state)
+    ? [nextHead(state)].concat(state.snake)
+    : [nextHead(state)].concat(removeLast(state.snake))
+}
+
+const nextMoves = (state: State): Move[] =>
+  state.moves.length > 1 ? removeFirst(state.moves) : state.moves
 
 const next = (state: State): State => ({
   ...state,
   moves: nextMoves(state),
-  snake: nextSnake(state)
+  snake: nextSnake(state),
+  apple: nextApple(state)
 })
 
 const step = (t1: number) => (t2: number): void => {
@@ -86,14 +120,24 @@ const step = (t1: number) => (t2: number): void => {
 }
 
 const enqueue = (state: State, move: Move): State =>
-  isValidMove(state)(move) ? { ...state, moves: state.moves.concat([move]) } : state
+  isValidMove(state)(move)
+    ? { ...state, moves: state.moves.concat([move]) }
+    : state
 
 window.addEventListener('keydown', event => {
   switch (event.key) {
-    case 'ArrowUp': state = enqueue(state, UP); break
-    case 'ArrowDown': state = enqueue(state, DOWN); break
-    case 'ArrowRight': state = enqueue(state, RIGHT); break
-    case 'ArrowLeft': state = enqueue(state, LEFT); break
+    case 'ArrowUp':
+      state = enqueue(state, UP)
+      break
+    case 'ArrowDown':
+      state = enqueue(state, DOWN)
+      break
+    case 'ArrowRight':
+      state = enqueue(state, RIGHT)
+      break
+    case 'ArrowLeft':
+      state = enqueue(state, LEFT)
+      break
   }
 })
 
